2dbba11e3f31fe434fb5cb54332582c9df1cc42c
Pupilo <willianmayan@gmail.com>
2014-10-07 16:49:34 -0300

    Iniciando desenvolvimento com openstreetmap

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
 ** Helder Santana - helder.bs.santana@gmail.com
 **
 **/
+
 ?>
 <?php 
 include 'includes/autoload.php';
@@ -35,6 +36,9 @@ $grupo = new Conexao();
 $grupo->conectar();
 
 ?>
+
+<!-- =================================================================== -->
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -42,53 +46,32 @@ $grupo->conectar();
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <title>Geolocaliza&ccedil;&atilde;o - SSPDS/CE</title>
 <script type="text/javascript" src="js/jquery-1.7.2.js"></script>
-<!--
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>**/
--->
-<script src="https://maps.googleapis.com/maps/api/js?v=3.10&sensor=false&libraries=weather"></script>
-<script type="text/javascript" src="js/markerwithlabel.js"></script>
-<script type="text/javascript" src="js/markerwithlabel_packed.js"></script>
-<script type="text/javascript" src="js/oms.min.js"></script>
+
+<!-- CSS -->
 <link rel="stylesheet" type="text/css" href="css/geral.css" />
 <link rel="stylesheet" type="text/css" href="css/grupo.css" />
 
+
+<!-- ========================= OpenStreetMap =========================  -->
+<!--
+<script src="http://www.openlayers.org/api/OpenLayers.js"></script>
+<div id="#mapa" style="height:600px"></div>
+<script src="OpenLayers.js"></script>
 <script>
-
-function carregarGrupo() {
-	
-	var grupo	= $("#grupo").val();
-	var camada	= $("#camada").val();
-	
-	$.ajax({
-		type:	"POST",
-		url:	"grupo.php",
-		cache:  false,
-		data:	"grupo="+grupo+"&camada="+camada,
-	
-		beforeSend: function() {
-			$('#mapa').html("<div class='carregando'><div id='carregando_imagem'></div></div>");
-		},
-		success: function(retorno) {
-			$('#mapa').fadeIn(1000);
-			$("#mapa").html(retorno);
-		},
-	
-		error: function(txt) {
-		}
-	});
-}
-
-window.onload = function(){
-	carregarGrupo();
-}
-
-</script>
-
+    map = new OpenLayers.Map("#mapa");
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.zoomToMaxExtent();
+    
+    map.addControl(new OpenLayers.Control.MousePosition());
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.zoomToMaxExtent();
+</script> 
+-->
+<!-- =================================================================== -->
 
 </head>
 
 <body>
-
 <div id="pagina-principal">
 
 	<table id="pagina-principal-tabela">
@@ -121,41 +104,32 @@ window.onload = function(){
 					?>
 					
 					</select>
-				</div>
-
-				<div id="camadas">
-
-				<?php
-				echo "<select id='camada'>";
-				echo "<option value=''>Escolha a camada desejada</option>";
-			        $ponteiro = fopen ("layers.conf","r");
-       				while (!feof ($ponteiro)) {
-             			  $linha = fgets($ponteiro,4096);
-             			  $tamLinha = strlen($linha);
-	     			  $posIgual = strpos($linha,'=');
-             			  echo "<option value='".substr($linha,$posLinha+1+$posIgual)."'>".substr($linha,0,$posIgual)."</option>";
-       				}//fim while
-				echo "</select>";
-				fclose ($ponteiro);
-				?>
-
-				</div>
-				
+				</div>			
 			</td>
 		</tr>
 		
 		<tr>
 			<td class="pagina-principal-centro">
-				<div id="mapa"></div>
+				<script src="http://www.openlayers.org/api/OpenLayers.js"></script>
+				<div id="OpenMap" style="height:600px"></div>
+				<script src="OpenLayers.js"></script>
+				<script>
+    				mapa = new OpenLayers.Map("OpenMap");
+    				mapa.addLayer(new OpenLayers.Layer.OSM());
+    				mapa.zoomToMaxExtent();
+    
+    				mapa.addControl(new OpenLayers.Control.MousePosition());
+    				mapa.addLayer(new OpenLayers.Layer.OSM());
+    				mapa.zoomToMaxExtent();
+				</script> 
+				</div>
 			</td>
 		</tr>
 	
 	</table>
 
 </div>
-
 </body>
-
 
 </html>
 
